9b912789e531125bdff3ce5bb063b752e0b57ef8
Inzamam <inzamam@credencesoft.in>
2022-08-10 17:02:42 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chatapp',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
